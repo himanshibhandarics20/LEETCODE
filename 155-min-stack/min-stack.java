15e1841a9f1089974207ea5d52121836d136ac1a
himanshibhandarics20 <himanshi.bhandari.cs27@iilm.edu>
2026-08-17 10:27:02 +0530

    Time: 36 ms (24.57%) | Memory: 100 MB (53.16%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> min = new Stack<>();
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (min.isEmpty() || val <= min.peek())
+            min.push(val);
+    }
+
+    public void pop() {
+        if (stack.peek().equals(min.peek()))
+            min.pop();
+
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
