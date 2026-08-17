02d7b0d5e3ccecc8d6ca0389e3db382273a9b577
himanshibhandarics20 <himanshi.bhandari.cs27@iilm.edu>
2026-08-17 09:56:39 +0530

    Time: 6 ms (28.57%) | Memory: 45.1 MB (37.07%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        return "/" + String.join("/", stack);
+    }
+}
